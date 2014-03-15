3cfd8adc9b067cc13e7886f8143fcb5e3023796c
JoeMu <jmuharsky@gmail.com>
2014-03-15 14:21:17 -0700

    * Factored out the view/state management into components.PageSectionStates. * Moved template files into /views folder (to accomodate additional files for controllers and tests). * Added GAE model/handler and Angular service for Locations data. * Added GAE model for Events data.

--- client/app.js
+++ client/app.js
@@ -1,6 +1,9 @@
 goog.provide('tripbuddy.app');
 goog.require('tripbuddy.components.event.EventDataService');
 goog.require('tripbuddy.components.event.EventListDirective');
+goog.require('tripbuddy.components.location.LocationDataService');
+goog.require('tripbuddy.components.location.list.LocationListDirective');
+goog.require('tripbuddy.components.PageSectionStates');
 goog.require('tripbuddy.core.page.PageDirective');
 goog.require('tripbuddy.core.page.PageHeaderDirective');
 goog.require('tripbuddy.core.page.PageStateService');
@@ -10,46 +13,17 @@ tripbuddy.app = angular.module('tripbuddy', ['ui.router', 'ui.bootstrap']);
 /** Registers services */
 tripbuddy.app.service('pageState', tripbuddy.core.page.PageStateService);
 tripbuddy.app.service('eventData', tripbuddy.components.event.EventDataService);
+tripbuddy.app.service('locationData', tripbuddy.components.location.LocationDataService);
 
 /** Register controllers */
-//tripbuddy.app.controller('pageCtrl', tripbuddy.core.page.PageController);
+tripbuddy.app.controller('pageCtrl', tripbuddy.core.page.PageController);
 
 /** Register directives */
 tripbuddy.app.directive('page', tripbuddy.core.page.PageDirective);
 tripbuddy.app.directive('pageHeader', tripbuddy.core.page.PageHeaderDirective);
 
 tripbuddy.app.directive('eventList', tripbuddy.components.event.EventListDirective);
+tripbuddy.app.directive('locationList', tripbuddy.components.location.list.LocationListDirective);
 
-tripbuddy.app.config(function($stateProvider, $locationProvider){
-    $stateProvider
-        .state('events', {
-            url: '/events',
-            views: {
-                'page.leftwell': { templateUrl: '/components/event/list/actions-view.html' },
-                'page.content': { templateUrl: '/components/event/list/content-view.html' }
-            }
-        })
-        .state('event', {
-            url: '/event/:eventId',
-            views: {
-                'page.leftwell': { templateUrl: '/components/event/detail/actions-view.html' },
-                'page.content': { templateUrl: '/components/event/detail/content-view.html' }
-            }
-        })
-        .state('locations', {
-            url: '/locations',
-            views: {
-                'page.leftwell': { templateUrl: '/components/location/list/actions-view.html' },
-                'page.content': { templateUrl: '/components/location/list/content-view.html' }
-            }
-        })
-        .state('location', {
-            url: '/location/:locationId',
-            views: {
-                'page.leftwell': { templateUrl: '/components/location/detail/actions-view.html' },
-                'page.content': { templateUrl: '/components/location/detail/content-view.html' }
-            }
-        });
-
-    $locationProvider.html5Mode(true)
-});
+/** Register UI and state routing */
+tripbuddy.app.config(tripbuddy.components.PageSectionStates);
